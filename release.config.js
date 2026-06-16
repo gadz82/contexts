0cac2b7988c768fa3412c77f3f4e3ecd2c519412
Francesco Marchesini <francesco.marchesini@docebo.com>
2026-06-16 17:25:56 +0200

    feat: contexts lib for AI context-management and switch
    Github actions setup

--- release.config.js
+++ release.config.js
@@ -3,6 +3,7 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", { npmPublish: false }],
     "@semantic-release/github",
   ],
 };
